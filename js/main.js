9328826186febc82bda04808ab31cb731d297a0d
unknown <jlfabilaceballos137@gmail.com>
2026-04-19 21:41:40 -0600

    Avance en el menu de productos e integracion del modal para consultar el mapa en la pagina principal. 19-04-2026

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
-// Funcion del boton para regresar el scroll.
-const btn = document.getElementById('botonSubir');
-
+// Evento del boton para regresar el scroll.
+const BTN_SUBIR = document.getElementById('botonSubir');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
-        btn.classList.add('visible');
+        BTN_SUBIR.classList.add('visible');
     } else {
-        btn.classList.remove('visible');
+        BTN_SUBIR.classList.remove('visible');
     }
 });
-
-btn.addEventListener('click', () => {
+BTN_SUBIR.addEventListener('click', () => {
     window.scrollTo({ top: 0});
 });
+
+// Funciones del modal para el mapa.
+function abrirModal() {
+    document.getElementById('fondoModal-mapa').classList.add('open');
+    document.body.style.overflow = 'hidden';
+    if (window.scrollY > 300) {
+        BTN_SUBIR.classList.remove('visible');
+    }
+}
+function cerrarModal() {
+    document.getElementById('fondoModal-mapa').classList.remove('open');
+    document.body.style.overflow = '';
+    if (window.scrollY > 300) {
+        BTN_SUBIR.classList.add('visible');
+    }
+}
+function cerrarConFondo(e) {
+    if (e.target === document.getElementById('fondoModal-mapa')) {
+    cerrarModal();
+    }
+}
