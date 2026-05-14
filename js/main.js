c758c55fbb676a1f71915b4a2b567725b8aeeee0
unknown <jlfabilaceballos137@gmail.com>
2026-05-14 02:44:37 -0600

    Actualizacion del funcionamiento de los modales. 14-05-2026

--- js/main.js
+++ js/main.js
@@ -11,23 +11,44 @@ BTN_SUBIR.addEventListener('click', () => {
     window.scrollTo({ top: 0});
 });
 
-// Funciones del modal para el mapa.
-function abrirModal() {
-    document.getElementById('fondoModal-mapa').classList.add('open');
+// Funciones de modal.
+function abrirModal(idModal) {
+    const modal = document.getElementById(idModal);
+    modal.classList.add('open');
     document.body.style.overflow = 'hidden';
     if (window.scrollY > 300) {
         BTN_SUBIR.classList.remove('visible');
     }
 }
-function cerrarModal() {
-    document.getElementById('fondoModal-mapa').classList.remove('open');
+function cerrarModal(idModal) {
+    const modal = document.getElementById(idModal);
+    modal.classList.remove('open');
     document.body.style.overflow = '';
     if (window.scrollY > 300) {
         BTN_SUBIR.classList.add('visible');
     }
 }
-function cerrarConFondo(e) {
-    if (e.target === document.getElementById('fondoModal-mapa')) {
-    cerrarModal();
+function cerrarConFondo(e, idModal) {
+    const modal = document.getElementById(idModal);
+    if (e.target === modal) {
+        cerrarModal(idModal);
     }
 }
+
+// Imagenes de la Galeria
+const imagenesGaleria = document.querySelectorAll('.galeria-item');
+const imagenModal = document.getElementById('galeria-imagen');
+const textoModal = document.getElementById('galeria-texto');
+imagenesGaleria.forEach(item => {
+    item.addEventListener('click', () => {
+        const imagen = item.querySelector('.galeria-imagen img');
+        imagenModal.src = imagen.src;
+
+        const texto = item
+            .querySelector('.galeria-overlay h4')
+            .innerText;
+        textoModal.innerText = texto;
+        
+        abrirModal('modal-galeria');
+    });
+});
